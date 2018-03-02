eed3f7fbec6a8247c9045dadbad748bc8e9503bc
lopze94 <lopze.94@gmail.com>
2018-03-02 01:20:46 -0700

    footer changes

--- vue-webpack/src/components/AppFooter.vue
+++ vue-webpack/src/components/AppFooter.vue
@@ -16,7 +16,7 @@
         </v-btn>
       </v-card-text>
       <v-card-text class="white--text pt-0">
-        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+download source code
       </v-card-text>
       <v-card-text class="white--text">
         &copy;2018 — <strong>Vuetify</strong>
