645107ae070c49479b0304c9cd094540f526095d
lopze94 <lopze.94@gmail.com>
2018-03-02 00:26:58 -0700

    edited Contac info

--- vue-webpack/src/components/Profile.vue
+++ vue-webpack/src/components/Profile.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
     <h1>Contact</h1>
-    <h2>{{ name }}</h2>
+    <h2>Email</h2>
+    <p>{{ email }}</p>
+    <h2>Phone</h2>
+    <p>{{ phone }}</p>
+    <h2>Address</h2>
+    <p>{{ address }}</p>
+    <p>{{ city }}</p>
+    <p>{{ state }} {{ zipCode }}</p>
     <img v-bind:src="imagePath"/>
   </div>
 </template>
@@ -11,7 +18,12 @@
    name: 'Profile',
    data () {
      return {
-       name: 'Road Runner',
+       email: 'info@culturabakery.com',
+       phone: '(801) 400-8794',
+       address: '151 N University Ave',
+       city: 'Provo',
+       state: 'UT', 
+       zipCode: '84601',
        imagePath: '/static/images/cultura.jpg'
      }
    }
