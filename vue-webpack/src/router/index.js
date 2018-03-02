113e6f87345152724099e3b3fec798af70ba96a0
lopze94 <lopze.94@gmail.com>
2018-03-01 23:54:13 -0700

    Fixed Menu

--- vue-webpack/src/router/index.js
+++ vue-webpack/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HomePage from '@/components/HomePage'
 import Profile from '@/components/Profile'
-import Todo from '@/components/Menu'
+import Menu from '@/components/Menu'
 
 Vue.use(Router)
 
